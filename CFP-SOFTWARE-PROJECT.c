1a24e9600022e430d088765d10816fd78e7af9d0
JaShAce <118231692+JaShAce@users.noreply.github.com>
2022-11-22 23:21:02 +0800

    Update CFP-SOFTWARE-PROJECT.c

--- CFP-SOFTWARE-PROJECT.c
+++ CFP-SOFTWARE-PROJECT.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
 
-int GradeInput(int Grade); // Grade Input
-int GWA(int CourseGrades1[12], int CourseGrades2[11]); // GWA Calc
+int GPA(int CourseGrades1[], int CourseGrades2[]); // GPA Calc semestral no unit
+int GWA(int CourseGrades1[], int CourseGrades2[]); // GWA Calc with unit weight
 int CAD(); // Cad Checker
 
 int main () {
 	char name[50];
 	char CourseNames1[12][10] = // first semester grade 12 subjects
-	{"CCF 03", "BIO 01", "BIO 01L", "EIP 01S", 
-	 "ELEC 01", "ENGL 01", "LIT 01", "PE 03", 
-	 "PHYS 01", "PHYS 01L", "REED 03", "RES 02S"};	
+	{"CCF 03", "BIO 01", "BIO 01L", "EIP 01S", "ELEC 01", "ENGL 01", "LIT 01", "PE 03", "PHYS 01", "PHYS 01L", "REED 03", "RES 02S"};
 	char CourseNames2[11][10] = // second semester grade 12 subjects
-	{"CCF 04", "BIO 02", "BIO 02L", "CONARTS 01", "ENTREP 01", ""}; // add more subjects refer to gc for list
+	{"CCF 04", "BIO 02", "H", "L", "N", "AWDJJI"};
 	int i, regStudent, GradeAverage, CourseGrades1[12], CourseGrades2[11];
-	size_t m = sizeof(CourseGrades1)/sizeof(int);
-	size_t n = sizeof(CourseGrades2)/sizeof(int);	
+	size_t m = (sizeof(CourseGrades1)/sizeof(int)) - 1;
+	size_t n = (sizeof(CourseGrades2)/sizeof(int)) - 1;	
 	
-	
+	for(i=0;i<=m;i++) {
+		printf("%s\n", CourseNames1[i]);
+	}
+	for(i=0;i<=n;i++) {
+		printf("%s\n", CourseNames2[i]);
+	}
+		
 	printf("What is your full name? ");
 	scanf("%s", &name);
 	printf("Are you a regular student? Type \"1\" if yes and \"0\" if no: ");
@@ -33,42 +37,60 @@ int main () {
 		scanf("%d", &CourseGrades2[i]);
 	}
 	
+	GPA(CourseGrades1, CourseGrades2);
 	GWA(CourseGrades1, CourseGrades2);
 	
 	return 0;
 }
 
-int GWA(int CourseGrades1[12], int CourseGrades2[11]) { // Calculate GWA
-	int i, sum1, sum2, avg1, avg2;
-	size_t m = sizeof(CourseGrades1)/sizeof(int);
-	size_t n = sizeof(CourseGrades2)/sizeof(int);	
+int GPA(int CourseGrades1[], int CourseGrades2[]) { // Calculate GWA
+	int i, avg1, avg2;	
+	int sum1 = 0;
+	int sum2 = 0;
 	
+		
+	// Average first semester
+	for(i=0; i<=11; i++) {
+		sum1 = sum1 + CourseGrades1[i];
+	}
+	avg1 = sum1 / 12;
+	// Average second semester
+	for(i=0; i<=10; i++) {
+		sum2 = sum2 + CourseGrades2[i];
+	}
+	avg2 = sum2 / 11;
+
+
+	printf("%d\n", sum1);
+	printf("%d\n", avg1);	
+	printf("%d\n", sum2);
+	printf("%d\n", avg2);
+}
+
+int GWA(int CourseGrades1[], int CourseGrades2[]) {
+	int i, avg1, avg2, unitcount1, unitcount2;	
+	int sum1 = 0, sum2 = 0, unitcount1 = 0, 
 	// first semester grade 12 subject unit score
 	int CourseUnits1[12] = {0, 1, 3, 3, 3, 3, 3, 1, 3, 1, 3, 3};
 	// second semester grade 12 subject unit score
 	int CourseUnits2[11] = {0, 1, 3, 3, 3, 3, 1, 3, 1, 3, 3};
-	 	 
-	printf("test\n");
-	
+
 	// Average first semester
-	for(i=0; i<=m; i++) {
-		sum1 = sum1 + CourseGrades1[i];
-		avg1 = sum1/i;
+	for(i=0; i<=11; i++) {
+		sum1 = sum1 + (CourseGrades1[i] * CourseUnits1[i]);
 	}
-	printf("%d", sum1);
-	printf("%d", avg1);
-	
+	avg1 = sum1 / 12;
 	// Average second semester
-	for(i=0; i<=n; i++) {
-		sum2 = sum2 + CourseGrades2[i];
-		avg2 = sum2/i;
+	for(i=0; i<=10; i++) {
+		sum2 = sum2 + (CourseGrades2[i] * CourseUnits2[i]);
+		unitcount2 = unitcount2 + ;
 	}
-	printf("%d", sum2);
-	printf("%d", avg2);
-	
-	// GWA/GPA(i forgor which one) - both semesters together average
-	
-	
+	avg2 = sum2 / ;
+
+	printf("%d\n", sum1);
+	printf("%d\n", avg1);	
+	printf("%d\n", sum2);
+	printf("%d\n", avg2);
 }
 
 int CAD(int GWA) { // Check if passes CAD requirements
@@ -89,4 +111,3 @@ int CAD(int GWA) { // Check if passes CAD requirements
 	
 	return 0;
 }
-
