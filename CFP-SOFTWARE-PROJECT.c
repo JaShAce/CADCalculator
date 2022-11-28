129e31a98afc731731cbbfac898317df3040939a
JaShAce <118231692+JaShAce@users.noreply.github.com>
2022-11-28 21:07:56 +0800

    Update CFP-SOFTWARE-PROJECT.c
    changed some int to float type, module comment titles, other comments n stuff and other general integration sht

--- CFP-SOFTWARE-PROJECT.c
+++ CFP-SOFTWARE-PROJECT.c
@@ -1,70 +1,133 @@
 #include <stdio.h>
 
 // functions
-void GPA(int* avg1, int* avg2); // GPA Calc semestral no unit
-void GWA(int GPA1, int GPA2, int* WeightedAve); // GWA Calc with unit weight
-int CAD(int GWA, int regStudent); // Cad Checker
+void GPA(float* avg1, float* avg2); // GPA Calc semestral no unit
+void GWA(float GPA1, float GPA2, float* WeightedAve); // GWA Calc with unit weight
+int CAD(float GWA, int regStudent); // CAD Checker
 
 // global variables
-int CourseGrades1[12], CourseGrades2[11];
+float CourseGrades1[12], CourseGrades2[11];
 const size_t m = sizeof(CourseGrades1)/sizeof(int) - 1, n = sizeof(CourseGrades2)/sizeof(int) - 1; 
 	
 	
+/* 
+===============================================================
+FILE: GRADE 12 CAD AWARDEE CHECKER BASED ON GENERAL WEIGHTED AVERAGE SOFTWARE.c
+AUTHORS: Derama, Jeskha Samantha; Acebes, Jared Sheohn; Bitero, John Christion; Lllanto, Miguel Anthony;
+GRADE LEVEL & BLOCK: STEM 12-T2
+DESCRIPTION: This function asks the user’s name, if they’re a regular grade 12 student, input grades, calls the other functions, and display outputs.
+COPYRIGHT: November 28, 2022
+===============================================================
+*/ 	
 	
 int main () {	
-	const char CourseNames1[12][10] = // first semester grade 12 subjects
+	const char CourseNames1[12][10] = 	// Declares first semester grade 12 subjects
 	{"CCF 03", "BIO 01", "BIO 01L", "EIP 01S", 
 	 "ELEC 01", "ENGL 01", "LIT 01", "PE 03", 
 	 "PHYS 01", "PHYS 01L", "REED 03", "RES 02S"};
-	const char CourseNames2[11][11] = // second semester grade 12 subjects
+	const char CourseNames2[11][11] = 	// Declares second semester grade 12 subjects
 	{"CCF 04", "BIO 02", "BIO 02L", "CONARTS 01", "ENTREP 01", "MIL 01", "PE 04", "PHYS 02", 
 	 "PHYS 02L", "REED 04", "RES 03S"};
 	
+	// Variable Declarations
 	char name[50];
-	int i, regStudent, GPAve[2], GWAve;
+	int i, regStudent;
+	float GPAve[2], GWAve;
 	
-	// input	
+	// Title Page
+	
+	// Reads the name of the user	
 	printf("What is your full name? ");
 	fgets(name, 50, stdin);
-	printf("Are you a regular student? Type \"1\" if yes and \"0\" if no: ");
-	scanf("%d", &regStudent);
 	
-
-	for(i=0;i<=m;i++) { // Input Grades First Semester
+	
+	// Asks if the user is a regular student or not, as one of the requirements for the CAD Award
+	printf("\nAre you a regular student? Type \"0\" if no and any integer if yes (e.g. \"1\"): ");	
+	while(scanf("%d", &regStudent)!=1) { 	// Scans input and validates if an integer
+	    printf("\nInvalid input, please enter an integer: ");
+	    scanf("%s");
+    } 
+    // Identify if the user is a regular student
+	if (regStudent != 0){ 
+    	printf("\n\nYou are a regular student.\n");
+	}
+	else {
+		printf("\n\nYou are an irregular student.\n");
+	}
+	
+	printf("\n...Press Any Key to Continue:");
+	getch();
+	system("cls");
+	
+	printf("=================================================================\n");
+	
+	// Declares program limitations
+	printf("\nImportant Note: If the input is invalid (e.g. not an integer), \nthe program will automatically declare you ineligible for CAD Award.\n\n");
+	
+	printf("=================================================================\n");
+	
+	// Gives instructions to user
+	printf("\nEnter first semester grades\n\n");
+	
+	// Input First Semester Grades
+	for(i=0;i<=m;i++) { 
 		printf("Input Grade for %s: ", CourseNames1[i]);
-		scanf("%d", &CourseGrades1[i]);
+		scanf("%f", &CourseGrades1[i]);
 	}
-	for(i=0;i<=n;i++) { // Input Grades Second Semester
+	
+	printf("\n\n");
+	
+	// Gives instructions to user
+	printf("Enter second semester grades\n\n");
+
+	// Input Second Semester Grades
+	for(i=0;i<=n;i++) { 
 		printf("Input Grade for %s: ", CourseNames2[i]);
-		scanf("%d", &CourseGrades2[i]);
+		scanf("%f", &CourseGrades2[i]);
 	}
+	
+	// Calls functions
 	GPA(&GPAve[0], &GPAve[1]);
 	GWA(GPAve[0], GPAve[1], &GWAve);
 	
-	// output
-	system("cls");
-	printf("Student Name: %s\n", name);
-	printf("First Semester GPA: %d\n", GPAve[0]);
-	printf("Second Semester GPA: %d\n", GPAve[1]);
-	printf("General Weighted Average (GWA): %d\n", GWAve);
-	if(CAD(GWAve, regStudent) == 1) {
-		printf("You are eligible for CAD");	
+	// Displys Outputs
+	system("cls"); 	// Clears the screen
+	printf("Student Name: %s\n", name); 	// Displays the student's name
+	printf("First Semester GPA: %.2f\n", GPAve[0]); 	// Displays the calculated first semester GPA
+	printf("Second Semester GPA: %.2f\n", GPAve[1]); 	// Displays the calculated second semester GPA
+	printf("General Weighted Average (GWA): %.2f\n", GWAve); 	// Displays the calculated GWA
+	// Displays eligibilty for CAD award
+	if(CAD(GWAve, regStudent) == 1) { 	
+		printf("\nYou are eligible for a CAD Award. Congratulations!");	
 	}
 	else {
-		printf("You are not eligible for CAD");
+		printf("\nYou are not eligible for a CAD Award.");
 	}
 	
 	return 0;
 }
 
-void GPA(int* avg1, int* avg2) { // Calculate GWA
-	int i, sum1 = 0, sum2 = 0;
+
+/*
+================================================================
+FUNCTION: GPA Function
+DESCRIPTION: This function calculates the first and second semester GPA.
+ARGUMENTS: This function includes the global variables avg1, and avg2.
+RETURNS: This returns the value of the first and second semester GPA.
+================================================================
+*/
+
+void GPA(float* avg1, float* avg2) { 
+	// Variable Declaratons
+	int i;
+	float sum1 = 0, sum2 = 0;
 			
 	// Average first semester
 	for(i=0; i<=m; i++) {
 		sum1 = sum1 + CourseGrades1[i];
 	}
 	*avg1 = sum1 / 12;
+	
 	// Average second semester
 	for(i=0; i<=n; i++) {
 		sum2 = sum2 + CourseGrades2[i];
@@ -74,9 +137,19 @@ void GPA(int* avg1, int* avg2) { // Calculate GWA
 	return;
 }
 
-void GWA(int GPA1, int GPA2, int* WeightedAve) {
-	int i, avg1, avg2, sum1 = 0, sum2 = 0, unitcount1 = 0, unitcount2 = 0;
 
+/*
+===============================================================
+FUNCTION: GWA Function
+DESCRIPTION: This function calculates the GWA using the GPA calculated from the GPA Function
+ARGUMENTS: This function uses the variables GPA1 (first semester) and GPA2 (second semester)
+RETURNS: This returns the GWA (General Weighted Average)
+===============================================================
+*/
+
+void GWA(float GPA1, float GPA2, float* WeightedAve) {
+	int i, unitcount1 = 0, unitcount2 = 0;
+	float avg1, avg2, sum1 = 0, sum2 = 0;
 	// first semester grade 12 subject unit score
 	const int CourseUnits1[12] = {0, 1, 3, 3, 3, 3, 3, 1, 3, 1, 3, 3};
 	// second semester grade 12 subject unit score
@@ -102,17 +175,27 @@ void GWA(int GPA1, int GPA2, int* WeightedAve) {
 	return;
 }
 
-int CAD(int GWA, int regStudent) { // Check if passes CAD requirements
+
+/*
+===============================================================
+FUNCTION: CAD Function
+DESCRIPTION: This function determines if the user is qualified for being a CAD Awardee using the criterion and qualifications set by the university.
+ARGUMENTS: This function includes the variables GWA and regStudent. 
+RETURNS: This function returns 1 or 0, which means if it returns 1, the student will be eligible for a CAD award. However, if it returns 0 then the user is ineligble for the CAD award.
+===============================================================
+*/
+
+int CAD(float GWA, int regStudent) { // Check if passes CAD requirements
 	int i;
 	
-	if(regStudent != 0 && GWA >= 90) {
+	if(regStudent != 0 && GWA >= 90 && GWA <= 100) {
 		for(i=0;i<=m;i++) {
-			if (CourseGrades1[i] < 85) {
+			if (CourseGrades1[i] < 85 || CourseGrades1[i] > 100 ) {
 				return 0;
 			}
 		}
 		for(i=0;i<=n;i++) {
-			if (CourseGrades2[i] < 85) {
+			if (CourseGrades2[i] < 85|| CourseGrades1[i] > 100 ) {
 				return 0;
 			}
 		}	
