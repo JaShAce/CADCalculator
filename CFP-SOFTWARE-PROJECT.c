abb59e2779484940a3e6517419f728a4e5d81a59
JaShAce <118231692+JaShAce@users.noreply.github.com>
2022-12-08 16:44:32 +0800

    balsl
    h

--- CFP-SOFTWARE-PROJECT.c
+++ CFP-SOFTWARE-PROJECT.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <windows.h>
 
-// functions
+
+
+// Declaring Functions
 void GPA(float* avg1, float* avg2); // GPA Calc semestral no unit
 void GWA(float GPA1, float GPA2, float* WeightedAve); // GWA Calc with unit weight
 int CAD(float GWA, int regStudent); // CAD Checker
 
-// global variables
+// Declaring Global Variables
 float CourseGrades1[12], CourseGrades2[11];
 const size_t m = sizeof(CourseGrades1)/sizeof(int) - 1, n = sizeof(CourseGrades2)/sizeof(int) - 1; 
 	
@@ -13,7 +16,7 @@ const size_t m = sizeof(CourseGrades1)/sizeof(int) - 1, n = sizeof(CourseGrades2
 /* 
 ===============================================================
 FILE: GRADE 12 CAD AWARDEE CHECKER BASED ON GENERAL WEIGHTED AVERAGE SOFTWARE.c
-AUTHORS: Derama, Jeskha Samantha; Acebes, Jared Sheohn; Bitero, John Christion; Lllanto, Miguel Anthony;
+AUTHORS: Derama, Jeskha Samantha; Acebes, Jared Sheohn; Bitero, John Christion; Llanto, Miguel Anthony
 GRADE LEVEL & BLOCK: STEM 12-T2
 DESCRIPTION: This function asks the user’s name, if they’re a regular grade 12 student, input grades, calls the other functions, and display outputs.
 COPYRIGHT: November 28, 2022
@@ -21,6 +24,10 @@ COPYRIGHT: November 28, 2022
 */ 	
 	
 int main () {	
+	// color
+	HANDLE hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	
 	const char CourseNames1[12][10] = 	// Declares first semester grade 12 subjects
 	{"CCF 03", "BIO 01", "BIO 01L", "EIP 01S", 
 	 "ELEC 01", "ENGL 01", "LIT 01", "PE 03", 
@@ -35,70 +42,147 @@ int main () {
 	float GPAve[2], GWAve;
 	
 	// Title Page
+	SetConsoleTextAttribute(hConsole, 15);
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("Welcome! \n\nThis software will calculate your GWA, and determine your eligibility for a CAD Award.\n"); //Tells the user what the software does
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("\n--\n\nTo use this software, press any key to continue:"); // Asks the user if they want to proceed
+	getch();
+	printf("\n\n...Press any key to confirm choice:"); // Asks the user to confirm their choice
+	getch();
+	system("cls"); // Clears screen
 	
+	
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("Section 1\n"); // Indicates what section the user is in
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("--\n");
+
 	// Reads the name of the user	
-	printf("What is your full name? ");
+	printf("\nEnter full name: ");
+	SetConsoleTextAttribute(hConsole, 11);
 	fgets(name, 50, stdin);
 	
 	
 	// Asks if the user is a regular student or not, as one of the requirements for the CAD Award
-	printf("\nAre you a regular student? Type \"0\" if no and any integer if yes (e.g. \"1\"): ");	
+	SetConsoleTextAttribute(hConsole, 15);
+	printf("\n--\n\nAre you a regular student? \nType \"0\" if no and any integer if yes (e.g. \"1\"): ");
+	SetConsoleTextAttribute(hConsole, 11);	
 	while(scanf("%d", &regStudent)!=1) { 	// Scans input and validates if an integer
+		SetConsoleTextAttribute(hConsole, 15);
 	    printf("\nInvalid input, please enter an integer: ");
 	    scanf("%s");
+	    SetConsoleTextAttribute(hConsole, 11);
     } 
     // Identify if the user is a regular student
+    SetConsoleTextAttribute(hConsole, 15);
 	if (regStudent != 0){ 
-    	printf("\n\nYou are a regular student.\n");
+    	printf("\nYou are a regular student.\n");
 	}
 	else {
-		printf("\n\nYou are an irregular student.\n");
+		printf("\nYou are an irregular student.\n");
 	}
 	
-	printf("\n...Press Any Key to Continue:");
+	printf("\n--\n\n...Press Any Key to Continue:"); // Asks the user if they want to proceed
 	getch();
-	system("cls");
+	system("cls"); // Clears screen
 	
-	printf("=================================================================\n");
-	
-	// Declares program limitations
-	printf("\nImportant Note: If the input is invalid (e.g. not an integer), \nthe program will automatically declare you ineligible for CAD Award.\n\n");
-	
-	printf("=================================================================\n");
-	
+	SetConsoleTextAttribute(hConsole, 15);
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("Section 2\n"); // Indicates what section the user is in
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("--\n");
+
 	// Gives instructions to user
-	printf("\nEnter first semester grades\n\n");
+	printf("\nEnter first semester grades\n\n--\n\n");
 	
 	// Input First Semester Grades
 	for(i=0;i<=m;i++) { 
+		SetConsoleTextAttribute(hConsole, 15);
 		printf("Input Grade for %s: ", CourseNames1[i]);
-		scanf("%f", &CourseGrades1[i]);
+		SetConsoleTextAttribute(hConsole, 11);
+		while(scanf("%f", &CourseGrades1[i])!=1 || CourseGrades1[i] <65 || CourseGrades1[i] >100){ // Validates input
+			fflush(stdin);
+			SetConsoleTextAttribute(hConsole, 15);
+			printf("Invalid input, please input a valid grade (between 65 and 100): ");
+			SetConsoleTextAttribute(hConsole, 11);
+		}
 	}
 	
-	printf("\n\n");
+	system("cls"); // Clears screen
+	
+	SetConsoleTextAttribute(hConsole, 15);
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("Section 3\n"); // Indicates what section the user is in
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("--\n");
 	
 	// Gives instructions to user
-	printf("Enter second semester grades\n\n");
+	printf("\nEnter second semester grades\n\n--\n\n");
 
 	// Input Second Semester Grades
 	for(i=0;i<=n;i++) { 
+		SetConsoleTextAttribute(hConsole, 15);
 		printf("Input Grade for %s: ", CourseNames2[i]);
-		scanf("%f", &CourseGrades2[i]);
+		SetConsoleTextAttribute(hConsole, 11);
+		while(scanf("%f", &CourseGrades2[i])!=1 || CourseGrades2[i] <65 || CourseGrades2[i] >100){ // Validates input
+			fflush(stdin);
+			SetConsoleTextAttribute(hConsole, 15);
+			printf("Invalid input, please input a valid grade (between 65 and 100): ");
+			SetConsoleTextAttribute(hConsole, 11);
+		}
 	}
 	
 	// Calls functions
 	GPA(&GPAve[0], &GPAve[1]);
 	GWA(GPAve[0], GPAve[1], &GWAve);
 	
-	// Displys Outputs
+	SetConsoleTextAttribute(hConsole, 15);
 	system("cls"); 	// Clears the screen
-	printf("Student Name: %s\n", name); 	// Displays the student's name
-	printf("First Semester GPA: %.2f\n", GPAve[0]); 	// Displays the calculated first semester GPA
-	printf("Second Semester GPA: %.2f\n", GPAve[1]); 	// Displays the calculated second semester GPA
-	printf("General Weighted Average (GWA): %.2f\n", GWAve); 	// Displays the calculated GWA
-	// Displays eligibilty for CAD award
+
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("Results Section\n"); // Indicates what section the user is in
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n--\n");
+	// Displays Outputs
+	printf("\nHere are the results, "); // Displays the student's name
+	SetConsoleTextAttribute(hConsole, 11);
+	printf("%s", name);
+	SetConsoleTextAttribute(hConsole, 15);
+	printf("\n--\n\n");
+	printf("First Semester GPA: "); // Displays the calculated first semester GPA
+	SetConsoleTextAttribute(hConsole, 11);
+	printf("%.2f\n", GPAve[0]);
+	SetConsoleTextAttribute(hConsole, 15);
+	printf("Second Semester GPA: "); // Displays the calculated second semester GPA
+	SetConsoleTextAttribute(hConsole, 11);
+	printf("%.2f\n", GPAve[1]);
+	SetConsoleTextAttribute(hConsole, 15);
+	printf("General Weighted Average (GWA): "); // Displays the calculated GWA
+	SetConsoleTextAttribute(hConsole, 11);
+	printf("%.2f\n", GWAve);
+	
+	int GW = (int)GWAve;
+	SetConsoleTextAttribute(hConsole, 15);
+	// Displays eligibilty for a CAD Award
 	if(CAD(GWAve, regStudent) == 1) { 	
-		printf("\nYou are eligible for a CAD Award. Congratulations!");	
+		printf("\nYou are eligible for a CAD Award ");	
+		if(GW >= 98) {
+			SetConsoleTextAttribute(hConsole, 14);
+			printf("with highest honors");
+		}
+		else if (GW >= 95) {
+			SetConsoleTextAttribute(hConsole, 10);
+			printf("with high honors");
+		}
+		else if (GW >= 90){
+			SetConsoleTextAttribute(hConsole, 13);
+			printf("with honors");
+		}
+		else {
+		 printf("invalid");
+		}
+		SetConsoleTextAttribute(hConsole, 15);
+		printf(". Congratulations!");
 	}
 	else {
 		printf("\nYou are not eligible for a CAD Award.");
@@ -148,28 +232,30 @@ RETURNS: This returns the GWA (General Weighted Average)
 */
 
 void GWA(float GPA1, float GPA2, float* WeightedAve) {
+	// Variable Declarations
 	int i, unitcount1 = 0, unitcount2 = 0;
 	float avg1, avg2, sum1 = 0, sum2 = 0;
-	// first semester grade 12 subject unit score
+	// First semester grade 12 subject unit score
 	const int CourseUnits1[12] = {0, 1, 3, 3, 3, 3, 3, 1, 3, 1, 3, 3};
-	// second semester grade 12 subject unit score
+	// Second semester grade 12 subject unit score
 	const int CourseUnits2[11] = {0, 1, 3, 3, 3, 3, 1, 3, 1, 3, 3};
 
 
-	// Average first semester w/out CCF
+	// Weighted Average for first semester
 	for(i=0; i<=m; i++) {
 		sum1 = sum1 + (CourseGrades1[i] * CourseUnits1[i]);
 		unitcount1 = unitcount1 + CourseUnits1[i];
 	}
 	avg1 = sum1 / unitcount1;
 	
-	// Average second semester w/out CCF
+	// Weighted Average for second semester
 	for(i=0; i<=n; i++) {
 		sum2 = sum2 + (CourseGrades2[i] * CourseUnits2[i]);
 		unitcount2 = unitcount2 + CourseUnits2[i];
 	}
 	avg2 = sum2 / unitcount2;
 	
+	// General Weighted Average of the two semesters
 	*WeightedAve = (avg1 + avg2) / 2;
 	
 	return;
@@ -184,22 +270,27 @@ ARGUMENTS: This function includes the variables GWA and regStudent.
 RETURNS: This function returns 1 or 0, which means if it returns 1, the student will be eligible for a CAD award. However, if it returns 0 then the user is ineligble for the CAD award.
 ===============================================================
 */
-
-int CAD(float GWA, int regStudent) { // Check if passes CAD requirements
+int CAD(float GWA, int regStudent) { 
+	// Variable declaration
 	int i;
 	
+	// Checks if the user is a regular student and examines if the GWA is within 90-100 
 	if(regStudent != 0 && GWA >= 90 && GWA <= 100) {
+		// Checks if the user has a grade in the first semester outside the grade range for the CAD award 
 		for(i=0;i<=m;i++) {
 			if (CourseGrades1[i] < 85 || CourseGrades1[i] > 100 ) {
 				return 0;
 			}
 		}
+		// Checks if the user has a grade in the second semester outside the grade range for the CAD award 
 		for(i=0;i<=n;i++) {
 			if (CourseGrades2[i] < 85|| CourseGrades1[i] > 100 ) {
 				return 0;
 			}
 		}	
+		// Returns 1 if the CAD requirements are met, thus user is eligible for a CAD award
 		return 1;	
 	}
+	// Returns 0 if the CAD requirements are not met, thus user is ineligible for a CAD award
 	return 0;
 }
